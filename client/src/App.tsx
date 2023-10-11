7a47b0873405c85448cda09f8f325c1502876926
keirrr <hadekshd@gmail.com>
2023-10-11 15:57:55 +0200

    Deploying app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ import HomePage from "./routes/HomePage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route exact path="/" element={<HomePage />} />
       <Route path="/meet/:id" element={<RenderAnswerMeeting />} />
       <Route path="/meet/new" element={<CreateMeeting />} />
       <Route path="*" element={<div>404</div>} />

8850cc022c3c61262f7018893e2b0d2e3a22e349
Kamil <hadekshd@gmail.com>
2023-11-13 13:05:07 +0100

    Update POST request

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -58,8 +58,8 @@ router.post("/new", async (req, res) => {
     await newMeet.save();
     return res.status(200).json({ message: "New appointment added", newMeet });
   } catch (err) {
-    console.warn(err);
-    return res.status(400).json({ message: "Invalid appointment" });
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 

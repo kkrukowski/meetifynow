4424a629df272ee8919e1dce1789626494b5ee92
Kamil <hadekshd@gmail.com>
2023-11-14 12:54:42 +0100

    Improve styles

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import meetifyNowLogo from "../assets/imgs/meetifynow-logo.webp";
 
 export default function Navbar() {
   return (
-    <nav className="absolute left-0 top-0 w-full p-8 flex align-center">
+    <nav className="absolute left-0 top-0 w-full p-5 md:p-8 flex align-center">
       <a href="/">
         <img src={meetifyNowLogo} alt="Logo" className="h-8" />
       </a>

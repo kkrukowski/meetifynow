8b52ae024fdace181c7ee84c4602aabf3464cb9b
Kamil <hadekshd@gmail.com>
2023-11-18 19:25:15 +0100

    Add footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,16 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col justify-center items-center absolute left-0 bottom-0 w-full p-5">
-      <p className="text-dark">
-        <a href="https://github.com/kkrukowski">
-          @kkrukowski
-          <span className="ml-2">
-            <FontAwesomeIcon icon={faGithub} />
-          </span>
-        </a>
-      </p>
-      <p className="text-center text-dark text-sm">2023 © meetifynow.com</p>
+    <div className="flex flex-col justify-center items-center absolute left-0 bottom-0 w-full p-2">
+      <div className="flex flex-col justify-center items-center w-fit">
+        <div className="w-full h-px bg-dark mb-2"></div>
+        <p className="text-dark">
+          <a href="https://github.com/kkrukowski">
+            @kkrukowski
+            <span className="ml-2">
+              <FontAwesomeIcon icon={faGithub} />
+            </span>
+          </a>
+        </p>
+        <p className="text-center text-dark text-sm">2023 © meetifynow.com</p>
+      </div>
     </div>
   );
 }

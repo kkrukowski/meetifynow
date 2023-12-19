9938b55808e04ec6a742e04f2b9a03bfe65953cd
Kamil <hadekshd@gmail.com>
2023-12-19 14:41:18 +0100

    feat: add dynamic titles

--- client/src/app/[lang]/meet/[id]/page.tsx
+++ client/src/app/[lang]/meet/[id]/page.tsx
@@ -2,7 +2,32 @@ import { getDictionary } from "@/lib/dictionary";
 import AnswerMeeting from "@/routes/AnswerMeeting";
 import { Locale } from "@root/i18n.config";
 import axios from "axios";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+let meetingData;
+
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const url = process.env.NEXT_PUBLIC_SERVER_URL + `/meet/${id}`;
+  meetingData = await axios
+    .get(url)
+    .then((res) => res)
+    .catch((err) => err.response);
+
+  let title = "Answer Not Found - MeetifyNow";
+
+  if (meetingData.status === 200) {
+    title = meetingData.data.meetName + ` - MeetifyNow`;
+  }
+
+  return {
+    title: title,
+  };
+}
 
 export default async function Page({
   params: { lang, id },
--- client/src/app/[lang]/meet/new/page.tsx
+++ client/src/app/[lang]/meet/new/page.tsx
@@ -2,6 +2,20 @@ import { getDictionary } from "@/lib/dictionary";
 import CreateMeeting from "@/routes/CreateMeeting";
 import { Locale } from "@root/i18n.config";
 
+export async function generateMetadata({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dict = await getDictionary(lang);
+
+  const title = dict.page.createMeeting.title + ` - MeetifyNow`;
+
+  return {
+    title,
+  };
+}
+
 export default async function Page({
   params: { lang },
 }: {

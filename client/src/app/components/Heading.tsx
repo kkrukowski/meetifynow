e29a5c911d6acf6914231dd5377e505e0cd1f51f
Kamil <hadekshd@gmail.com>
2023-12-19 13:33:51 +0100

    fix: heading size element

--- client/src/app/components/Heading.tsx
+++ client/src/app/components/Heading.tsx
@@ -2,5 +2,5 @@ export default function Heading(props: { text: string; className?: string }) {
   const headingStyle =
     "text-xl lg:text-2xl text-dark font-bold font-medium mb-5";
 
-  return <h3 className={headingStyle + " " + props.className}>{props.text}</h3>;
+  return <h2 className={headingStyle + " " + props.className}>{props.text}</h2>;
 }

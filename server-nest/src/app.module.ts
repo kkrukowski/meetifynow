0175223c63fc21d015f49e3acb940fc8cfcbf4de
Kamil <hadekshd@gmail.com>
2023-12-27 21:06:54 +0100

    feat: mongodb connection

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log('process.env.DB_CONN_URI', process.env.DB_CONN_URI);
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_CONN_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

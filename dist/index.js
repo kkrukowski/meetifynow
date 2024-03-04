320bfc980333d24f184566ae981f53bf3a4a478d
Kamil Krukowski <hadekshd@gmail.com>
2024-03-04 16:48:38 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-mongoose
-    .connect(process.env.DB_CONN_URI)
-    .then(() => console.log("DB Connected"))
-    .catch((err) => console.log(err));
-//# sourceMappingURL=index.js.map

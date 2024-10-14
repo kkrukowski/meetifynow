59f1d4520efa2992943f539d60ab4dfac1389834
Kamil Krukowski <hadekshd@gmail.com>
2024-10-14 13:53:22 +0200

    fix: server error

--- client/src/app/api/auth/getMeetData.ts
+++ client/src/app/api/auth/getMeetData.ts
@@ -5,6 +5,8 @@ import axios from "axios";
 export const getMeetData = async (id: string[], token: string) => {
     const url = process.env.NEXT_PUBLIC_SERVER_URL + `/meet/db/many`
 
+    console.log(url)
+
     const res = await axios.post(url, {
         headers: {
             'Authorization': `Bearer ${token}`
@@ -15,6 +17,12 @@ export const getMeetData = async (id: string[], token: string) => {
     }).then((res) => {
         return res.data
     })
+
+    console.log(res)
+
+    if (!res) {
+        return null
+    }
 
     return res
 }

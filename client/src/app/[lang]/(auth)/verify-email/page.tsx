38f5af6175f126383fe6c2fbf82881a60deec129
Kamil Krukowski <hadekshd@gmail.com>
2025-07-03 16:58:08 +0200

    Delete unused declarations

--- client/src/app/[lang]/(auth)/verify-email/page.tsx
+++ client/src/app/[lang]/(auth)/verify-email/page.tsx
@@ -1,18 +1,17 @@
-import RegisterPage from '@/routes/auth/RegisterPage';
-import {Locale} from "@root/i18n.config.ts";
-import {getDictionary} from "@/lib/dictionary.ts";
-import {auth} from "@src/auth.ts";
-import {redirect} from "next/navigation";
+import { getDictionary } from "@/lib/dictionary.ts";
 import VerifyEmail from "@/routes/auth/VerifyEmail.tsx";
-import {useLocation} from "react-router-dom";
-import {verifyEmail} from "@/api/verifyemail.ts";
+import { Locale } from "@root/i18n.config.ts";
+import { auth } from "@src/auth.ts";
+import { redirect } from "next/navigation";
 
-export default async function Page({params: { lang }}: { params: { lang: Locale } }) {
-    const session = await auth()
-    if (session?.user) return redirect("/profile")
-    const dict = await getDictionary(lang)
+export default async function Page({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const session = await auth();
+  if (session?.user) return redirect("/profile");
+  const dict = await getDictionary(lang);
 
-    return (
-        <VerifyEmail dict={dict}/>
-    )
+  return <VerifyEmail dict={dict} />;
 }

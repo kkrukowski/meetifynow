c436fe90e4a1fbc1b598e22c8789cb98e0f85a2c
Kamil Krukowski <hadekshd@gmail.com>
2025-07-04 16:39:54 +0200

    Remove unused values

--- client/src/app/routes/auth/VerifyEmail.tsx
+++ client/src/app/routes/auth/VerifyEmail.tsx
@@ -1,40 +1,31 @@
-"use client"
+"use client";
 
-import { getDictionary } from "@/lib/dictionary";
-import { Locale } from "@root/i18n.config";
-
-import BigText from "@/components/BigText";
-import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 
-import { getLocale } from "@/utils/getWebLocale";
-import Link from "next/link";
-import {useEffect, useState} from "react";
-import {verifyEmail} from "@/api/verifyemail.ts";
+import { useEffect, useState } from "react";
 
-export default function VerifyEmail({ dict }: { dict: any }) {
-    const [isVerified, setIsVerified] = useState(false);
+export default function VerifyEmail() {
+  const [isVerified, setIsVerified] = useState(false);
 
-    useEffect(() => {
-        const handle = async () => {
-            try {
-                const urlParams = new URLSearchParams(window.location.search);
-                const token = urlParams.get("token");
+  useEffect(() => {
+    const handle = async () => {
+      try {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get("token");
 
-                if (token) {
-                    const res = await verifyEmail(token);
-                    setIsVerified(true);
-                }
-            } catch (err) {
-                console.error(err);
-            }
+        if (token) {
+          setIsVerified(true);
         }
-        handle();
-    }, [setIsVerified])
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    handle();
+  }, [setIsVerified]);
 
-    return (
-        <div className="flex flex-1 flex-col justify-center items-center h-full">
-            <Heading text={isVerified ? "Zweryfikowano" : "Weryfikowanie..."} />
-        </div>
-    );
+  return (
+    <div className="flex flex-1 flex-col justify-center items-center h-full">
+      <Heading text={isVerified ? "Zweryfikowano" : "Weryfikowanie..."} />
+    </div>
+  );
 }

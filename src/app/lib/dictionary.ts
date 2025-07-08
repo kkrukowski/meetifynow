e9b60c0f32b7450fc36be0536552def286e7c317
Kamil Krukowski <hadekshd@gmail.com>
2025-07-08 12:37:42 +0200

    Fixes

--- src/app/lib/dictionary.ts
+++ src/app/lib/dictionary.ts
@@ -1,9 +1,13 @@
 import type { Locale } from "../../../i18n.config";
+import { i18n } from "../../../i18n.config";
 
 const dictionaries = {
   en: () => import("../dictionaries/en.json").then((module) => module.default),
   pl: () => import("../dictionaries/pl.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale as keyof typeof dictionaries]();
+export const getDictionary = async (locale: Locale) => {
+  const selectedLocale =
+    locale && locale in dictionaries ? locale : i18n.defaultLocale;
+  return dictionaries[selectedLocale as keyof typeof dictionaries]();
+};
